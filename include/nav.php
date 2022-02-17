3d3842ef0a15c533344606f5eac75ec2a4e4355c
Nirakar Bogati <lmntrix.as619@gmail.com>
2022-02-17 21:09:36 +0545

    hi

--- include/nav.php
+++ include/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,15 +22,14 @@
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-        <!-- </li>
-        <li class="nav-item">
+        </li>
+        <!-- <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li> -->
-        
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-light" type="submit">Search</button>
+        <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
