5662db4b50ea431f8e2d70850eadb7503a58d283
Nirakar Bogati <lmntrix.as619@gmail.com>
2022-02-19 20:10:48 +0545

    updated

--- home.php
+++ home.php
@@ -14,18 +14,18 @@ $data = mysqli_fetch_assoc($result);
 ?>
 
 <html>
-  <head>
-      <title>Home-Asmt News</title>
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  </head>
-<body>
-<?php include('include/nav.php');?>    
- 
-<div class="container">
-   <h3>Login by user <?php  echo $data['fullName']; ?></h3>
-</div>
+        <head>
+            <title>Home-Asmt News</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        </head>
+      <body>
+            <?php include('include/nav.php');?>    
+            
+            <div class="container">
+              <h3>Login by user <?php  echo $data['fullName']; ?></h3>
+            </div>
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 </html>
