eaaff56de5a0f7250ec6f6b2066b54d4c0d07862
Nirakar Bogati <lmntrix.as619@gmail.com>
2022-02-27 14:48:32 +0545

    confirm addeed

--- category.php
+++ category.php
@@ -56,7 +56,7 @@
              <?php while($row=mysqli_fetch_assoc($categoryResult)) { ?>         
            <tr>            
             <td><?php echo $row['title'];?></td>
-             <td> <a href="db/delete-category.php?id=<?php echo $row['id'];?>"><i class="fas fa-trash" style="color:red;"></i> </a>| E</td>
+             <td> <a href="#" onclick= "deleteConfirmation(<?php echo $row['id'];?>);"><i class="fas fa-trash" style="color:red;"></i> </a>| E</td>
            </tr>
            <i class="fa-solid fa-trash-can"></i>
            <?php } ?>
@@ -68,14 +68,29 @@
       </div>
     </div>
   </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/998a7629ba.js" crossorigin="anonymous"></script>
-<script src="js/boot.min.js"></script>
-<script>
-  function deleteConfirmation(){
-  bootbox.confirm("Are you sure?", function(result){
+
+
+
+  <script>
+  function deleteConfirmation(id){
+bootbox.confirm({ 
+    size: "small",
+    message: "Are you sure?",
+    callback: function(result){ 
+    if(result){
+      window.location = 'db/delete-category.php?id='+id;
+    }
+    }
 })
   }
+  
 </script>
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/998a7629ba.js" crossorigin="anonymous"></script>
+<script src="js/bootbox.min.js"></script>
+
+
+
 </body>
 </html>
