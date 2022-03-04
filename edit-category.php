a9deb6c90900fd2230e6c276134cb36e14c77c3d
Nirakar Bogati <lmntrix.as619@gmail.com>
2022-03-04 14:47:37 +0545

    updated

--- edit-category.php
+++ edit-category.php
@@ -6,25 +6,25 @@ if (!isset($_GET['id'])) {
 $cid= $_GET['id'];
 
 
-session_start();
- if(!isset($_SESSION['login']) || !$_SESSION['login']==1){
-   header('Location:login.php');
- }
- $id = $_SESSION['user_id']; 
- include('db/connect.php');
- $query = "SELECT * FROM users WHERE id='$id'";
-$result = mysqli_query($conn,$query);
-$data = mysqli_fetch_assoc($result);
+    session_start();
+    if(!isset($_SESSION['login']) || !$_SESSION['login']==1){
+    header('Location:login.php');
+    }
+    $id = $_SESSION['user_id']; 
+    include('db/connect.php');
+    $query = "SELECT * FROM users WHERE id='$id'";
+    $result = mysqli_query($conn,$query);
+    $data = mysqli_fetch_assoc($result);
 
-$categoryQuery="SELECT * FROM category WHERE id='$cid' ";
-$categoryResult=mysqli_query($conn, $categoryQuery);
+    $categoryQuery="SELECT * FROM category WHERE id='$cid' ";
+    $categoryResult=mysqli_query($conn, $categoryQuery);
 
-if (mysqli_num_rows($categoryResult)==0) {
-    die("No record found with this id");
-}
-else {
-    $row = mysqli_fetch_assoc($categoryResult);
-}
+    if (mysqli_num_rows($categoryResult)==0) {
+        die("No record found with this id");
+    }
+    else {
+        $row = mysqli_fetch_assoc($categoryResult);
+    }
 
 ?>
 
@@ -67,21 +67,19 @@ else {
 
 
 </body>
-<!-- jquery -->
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-<!-- bootstrap js                      -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
-<!-- fontawesome -->
+
 <script src="https://kit.fontawesome.com/6f38b1151d.js" crossorigin="anonymous"></script>
 
 
-<!-- bootbox -->
 <script src="js/bootbox.min.js"></script>
 
-<!-- check js something something😒 -->
 <script>
 function deleteConfirmation(id) {
     bootbox.confirm({
